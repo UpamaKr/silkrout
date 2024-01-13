6fde823a359bbc2f4accbb3a8a5960f09dc22ab1
UpamaKr <upama.sarker@yahoo.com>
2024-01-13 23:30:09 +0100

    cart bugs are added

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 
 # Create your views here.
 
@@ -31,11 +33,16 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        cart_id = _cart_id(request)
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=cart_id, product=single_product).exists()
+
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
     return render(request, 'store/product_detail.html', context)
